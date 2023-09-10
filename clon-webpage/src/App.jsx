30968122d3cb6852256f89056feffdbcfc59d404
Sebastian Rakauskas <sebastianalberto258@hotmail.com>
2023-09-10 17:21:44 -0500

    first version header

--- clon-webpage/src/App.jsx
+++ clon-webpage/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/header/Header';
 import Testimonio from './components/Testimonio.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Testimonio />
     </div>
   )
